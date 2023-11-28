19608bc49a781806470bdff7cce8d30417beefe0
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-11-28 05:51:01 -0800

    prints a string in uppercase followed by a new line

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
-        # Check if the character is lowercase
         if 'a' <= char <= 'z':
-            # Convert the lowercase character to uppercase using ASCII values
-            print(chr(ord(char) - 32), end='')
+            print("{}".format(chr(ord(char) - 32)), end='')
         else:
-            print(char, end='')
+            print("{}".format(char), end='')
 
     print()
