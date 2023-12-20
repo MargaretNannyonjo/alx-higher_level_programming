2184d140957118e9ac892e9bbce26dca8786066d
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-12-20 13:09:03 -0800

    Classes and Objects

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-# Print the alphabet in lowercase
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
--- 0x04-python-more_data_structures/3-common_elements.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def common_elements(set_1, set_2):
-    # Find the common elements
-    common_set = set_1.intersection(set_2)
-    return common_set
