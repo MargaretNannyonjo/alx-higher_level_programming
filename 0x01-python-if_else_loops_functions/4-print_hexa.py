dc552c9381d34b637760cf202bbb055a9b50a74d
MargaretNannyonjo <nannyonjoleon@gmail.com>
2024-01-02 05:16:50 -0800

    Python test driven development

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-# Printing numbers from 0 to 98 in decimal and hexadecimal
-for i in range(99):
-    print("{} = 0x{:x}".format(i, i))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    # print sorted name from directory
-    for name in sorted(dir(hidden_4)):
-        # print only names that don't start with _
-        if name[:1] != '_':
-            print("{}".format(name))
