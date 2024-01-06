fcfc359b01c8f4f2247e8e5f2f21aa9c71f38d77
MargaretNannyonjo <nannyonjoleon@gmail.com>
2024-01-06 00:53:51 -0800

    Python test driven development

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,18 +1,28 @@
-#include <Python.h>
+#include "Python.h"
 
 /**
- * print_python_string - Prints information about Python string objects
- * @p: PyObject string to analyze
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
  */
-void print_python_string(PyObject *p) {
-    if (!PyUnicode_Check(p)) {
-        printf("[.] string object info\n");
-        printf("  [ERROR] Invalid String Object\n");
-        return;
-    }
+void print_python_string(PyObject *p)
+{
+	long int length;
 
-    printf("[.] string object info\n");
-    printf("  type: compact unicode object\n");
-    printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
-    printf("  value: %ls\n", PyUnicode_AsWideCharString(p, NULL));
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
